3c2396407594ef5d1aec1e84320baf5c72b803cc
electricalfaris <electricalfaris@gmail.com>
2024-03-20 19:50:17 +0300

    sdf

---
+++ pages/spot_wilder_1/script.js
@@ -0,0 +1,19 @@
+// pages/Transformer_to_Solder_gun/script.js
+function getTemplateRecord(no) {
+  return `
+<div class="col-sm-6 col-md-4 mb-3 img-wrapper">
+ <img
+   src="../../images/spot_welder_1/${no}.png"
+   alt="${no}.png" class="fluid img-thumbnail">
+ </div>`;
+}
+onload = () => {
+  console.log("onload()...");
+  var root = document.querySelector("#root");
+
+  const imagesSum = 74;
+  for (let i = 1; i < imagesSum; i++) {
+    console.log(getTemplateRecord(i));
+    root.insertAdjacentHTML("beforeend", getTemplateRecord(i));
+  }
+};
